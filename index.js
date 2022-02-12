72615b26b18896d7a6da3b8a89f70cb4234a5332
PipHarsveld <pip.harsveld@outlook.com>
2022-02-12 20:13:55 +0100

    express gedownload

---
+++ index.js
@@ -0,0 +1,13 @@
+const camelCase = require('camelcase');
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
+
+
+console.log(camelCase('foo-bar'));
