3231274e762ff8a6424f38ce5fbe0ab870cde0ad
Pip <54938035+PipHarsveld@users.noreply.github.com>
2022-02-28 10:59:31 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const camelCase = require('camelcase');
-
 const express = require('express')
 const app = express()
+const { engine } = require( 'express-handlebars')
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+const PORT = 3000
+  app.use('/static',express.static('static'))
+  app.engine('handlebars', engine())
+  app.set('view engine', 'handlebars')
+  app.set('views', './views')
+
+  app.get('/', onhome)
+  app.get('/about', onabout)
+  app.get('/match', onmatch)
+
+function onhome(req, res){
+  res.render('home')
+}
+
+function onabout(req, res){
+  res.render('about')
+}
+
+function onmatch(req, res){
+  res.render('match')
+}
+
+app.use((req, res, next) => {
+  res.status(404).send('404 page not found')
 })
 
-app.listen(3000)
 
-
-console.log(camelCase('foo-bar'));
+app.listen(PORT, function() {
+  console.log("Port running on ", PORT)
+})
