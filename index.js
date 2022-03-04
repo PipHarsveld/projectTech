9d46ec220990e6a420b9cc40b02ab4586dac7143
PipHarsveld <pip.harsveld@outlook.com>
2022-03-04 09:21:02 +0100

    :white_check_mark: Body-parser toegevoegd

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const { engine } = require( 'express-handlebars')
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.post('/about',(req,res)=> {
+  res.send('Jouw username is '+req.body.user +' en je wachtwoord is '+req.body.password);
+});
+
+const multer  = require('multer')
+const upload = multer({ dest: 'static/uploads/' })
+
 const PORT = 3000
   app.use('/static',express.static('static'))
   app.engine('handlebars', engine())
@@ -29,6 +40,9 @@ app.use((req, res, next) => {
 })
 
 
+
+
+// Moet altijd onderaan blijven!
 app.listen(PORT, function() {
   console.log("Port running on ", PORT)
 })
