0a2d98b7006c741eddd0495d11eb97608ae1c7aa
PipHarsveld <pip.harsveld@outlook.com>
2022-03-11 12:26:15 +0100

    :boom: Connect to database and send info with Mongoose schema

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const express = require('express')
 const app = express()
 const { engine } = require( 'express-handlebars')
-
+const connectDB = require('./config/connect')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const restaurant = require('./models/restaurant')
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+require('dotenv').config()
 
-app.post('/like',(req,res)=> {
-  console.log(req.body);
-  res.send('Yes! Je hebt '+ req.body.userId + ' een ' + req.body.voorkeur + ' gegeven.');
-});
-
-app.post('/dislike',(req,res)=> {
-  console.log(req.body);
-  res.send('Ah, Je hebt '+ req.body.userId + ' een ' + req.body.voorkeur + ' gegeven.');
-});
+connectDB()
 
 const PORT = 3000
   app.use('/static',express.static('static'))
@@ -25,25 +21,100 @@ const PORT = 3000
   app.set('views', './views')
 
   app.get('/', onhome)
-  app.get('/about', onabout)
-  app.get('/match', onmatch)
+  app.get('/like', onlike)
+  app.get('/dislike', ondislike)
 
+app.post('/like',(req,res)=> {
+  console.log('Yes! Je hebt '+ req.body.naam + ' een ' + req.body.voorkeur + ' gegeven.');
+
+  run()
+  async function run() {
+    const Restaurant = require("./models/restaurant.js")
+    const restaurant = Restaurant.where("voorkeur").equals("like").select("id")
+    
+    try {
+      restaurant = await Restaurant.create({ 
+        naam : req.body.naam,
+        id: req.body.restaurantId,
+        voorkeur: req.body.voorkeur
+        // prijs: "€€",
+        // tags: ["Eetcafé", "Huiselijke sfeer", "Uitgeest"],
+        // omschrijving: ""
+        })
+        // await restaurant.save()
+        console.log(restaurant)
+      }
+      catch (e){
+          console.log(e.message)
+      }
+  }
+});
+
+app.post('/dislike',(req,res)=> {
+  console.log('Ahh, je hebt '+ req.body.naam + ' een ' + req.body.voorkeur + ' gegeven.');
+  
+  run()
+  async function run() {
+    const Restaurant = require("./models/restaurant.js")
+    const restaurant = Restaurant.where("voorkeur").equals("dislike").select("id")
+    
+    try {
+      restaurant = await Restaurant.create({ 
+        // Gegevens die verstuurd worden naar de database
+        naam : req.body.naam,
+        id: req.body.restaurantId,
+        voorkeur: req.body.voorkeur
+        })
+        console.log(restaurant)
+      }
+      catch (e){
+          console.log(e.message)
+      }
+  }
+});
+
+// Wordt getoond op de card, zichtbaar voor gebruiker
 function onhome(req, res){
   res.render('home', {
     restaurant1: {
       naam: "Eetcafé Klaas",
       prijs: "€€",
+      tags: [
+        "Eetcafé",
+        "Huiselijke sfeer",
+        "Uitgeest",
+      ],
       omschrijving: "Een fijne plek in Uitgeest om te genieten van goede wijnen, gin en tonics en mooie smakelijke gerechten voor een schappelijke prijs."
+    },
+    restaurant2: {
+      naam: "'t Schippersrijk",
+      prijs: "€€",
+      tags: [
+        "Bistro",
+        "Vega",
+        "Uitgeestermeer",
+      ],
+      omschrijving: "Kom langs en beleef onze gezellige sfeer aan het Uitgeestermeer! Een ongedwongen sfeer waar goed eten, lekker zitten en persoonlijke service op de absolute nummer één staan."
+    },
+    restaurant3: {
+      naam: "Spijkers",
+      prijs: "€€",
+      tags: [
+        "Bistro",
+        "Modern",
+        "Heemskerk",
+      ],
+      omschrijving: "Spijkers Heemskerk is de perfecte plek om volop te genieten van heerlijke gerechten tijdens een gezellige lunch of een mooie avond."
     }
   })
 }
 
-function onabout(req, res){
-  res.render('about')
+function onlike(req, res){
+  res.render('like')
 }
 
-function onmatch(req, res){
-  res.render('match')
+function ondislike(req, res){
+  res.render('dislike')
 }
 
 app.use((req, res, next) => {
