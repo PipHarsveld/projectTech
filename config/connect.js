b2924c56c28c55ccea2074c418adba2fd3fee08d
PipHarsveld <pip.harsveld@outlook.com>
2022-03-17 19:43:54 +0100

    :package: Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -4,15 +4,15 @@ const mongoose = require("mongoose");
 const url = process.env.MONGO_URI;
 
 const connectDB = () => {
-  try {
-    mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("DB is connected");
-  } catch (error) {
-    console.log("error");
-  }
+    try {
+        mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("DB is connected");
+    } catch (error) {
+        console.log("error");
+    }
 };
 
 module.exports = connectDB;
