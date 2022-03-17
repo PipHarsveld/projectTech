decc95d39396b10ad48c44921e47958ced8b77e0
PipHarsveld <pip.harsveld@outlook.com>
2022-03-17 19:44:50 +0100

    :art: Improve format of the code

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 const { engine } = require("express-handlebars");
 const connectDB = require("./config/connect");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const restaurant = require("./models/restaurant");
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
 require("dotenv").config();
@@ -26,81 +25,81 @@ app.get("/", onhome);
 
 // Wordt getoond op de card, zichtbaar voor gebruiker
 async function onhome(req, res) {
-  try {
+    try {
     // laat alleen de restaurants zien die de gebruiker nog niet heeft geswiped
-    const data = await restaurant.findOne({ voorkeur: "" }).exec();
-    res.render("home", { data: data });
-  } catch {
-    console.log("error");
-  }
+        const data = await restaurant.findOne({ voorkeur: "" }).exec();
+        res.render("home", { data: data });
+    } catch {
+        console.log("error");
+    }
 }
 
 // Wanneer gebruiker een card heeft geliked
 app.post("/like", async (req, res) => {
-  try {
-    restaurant.findOneAndUpdate({ voorkeur: "" }, { voorkeur: "like" }).exec();
-    const data = await restaurant.findOne({ voorkeur: "" }).exec();
-    res.render("home", { data: data });
-    console.log("like");
-  } catch {
-    console.log("fout bij liken");
-  }
+    try {
+        restaurant.findOneAndUpdate({ voorkeur: "" }, { voorkeur: "like" }).exec();
+        const data = await restaurant.findOne({ voorkeur: "" }).exec();
+        res.render("home", { data: data });
+        console.log("like");
+    } catch {
+        console.log("fout bij liken");
+    }
 });
 
 // Wanneer gebruiker een card heeft gedisliked
 app.post("/dislike", async (req, res) => {
-  try {
-    restaurant
-      .findOneAndUpdate({ voorkeur: "" }, { voorkeur: "dislike" })
-      .exec();
-    const data = await restaurant.findOne({ voorkeur: "" }).exec();
-    res.render("home", { data: data });
-    console.log("dislike");
-  } catch {
-    console.log("fout bij disliken");
-  }
+    try {
+        restaurant
+            .findOneAndUpdate({ voorkeur: "" }, { voorkeur: "dislike" })
+            .exec();
+        const data = await restaurant.findOne({ voorkeur: "" }).exec();
+        res.render("home", { data: data });
+        console.log("dislike");
+    } catch {
+        console.log("fout bij disliken");
+    }
 });
 
 // Toon lijst met gelikete restaurants
 app.post("/favorieten", async (req, res) => {
-  try {
-    const data = await restaurant.find({ voorkeur: "like" }).lean();
-    res.render("favorieten", { data: data });
-  } catch {
-    console.log("fout bij laden favorieten");
-  }
+    try {
+        const data = await restaurant.find({ voorkeur: "like" }).lean();
+        res.render("favorieten", { data: data });
+    } catch {
+        console.log("fout bij laden favorieten");
+    }
 });
 
 // Verwijder uit favorieten
 app.post("/verwijder", async (req, res) => {
-  try {
-    restaurant
-      .findOneAndUpdate({ voorkeur: "like" }, { voorkeur: "dislike" })
-      .exec();
-    const data = await restaurant.find({ voorkeur: "like" }).lean();
-    res.render("favorieten", { data: data });
-    console.log("dislike");
-  } catch {
-    console.log("fout bij verwijderen");
-  }
+    try {
+        restaurant
+            .findOneAndUpdate({ voorkeur: "like" }, { voorkeur: "dislike" })
+            .exec();
+        const data = await restaurant.find({ voorkeur: "like" }).lean();
+        res.render("favorieten", { data: data });
+        console.log("dislike");
+    } catch {
+        console.log("fout bij verwijderen");
+    }
 });
 
 // Refresh de database
 app.post("/refresh", async (req, res) => {
-  try {
-    restaurant.updateMany({}, { voorkeur: "" }).exec();
-    const data = await restaurant.findOne({ voorkeur: "" }).exec();
-    res.render("home", { data: data });
-  } catch {
-    console.log("fout bij refreshen");
-  }
+    try {
+        restaurant.updateMany({}, { voorkeur: "" }).exec();
+        const data = await restaurant.findOne({ voorkeur: "" }).exec();
+        res.render("home", { data: data });
+    } catch {
+        console.log("fout bij refreshen");
+    }
 });
 
-app.use((req, res, next) => {
-  res.status(404).send("404 page not found");
+app.use((req, res) => {
+    res.status(404).send("404 page not found");
 });
 
 // Moet altijd onderaan blijven!
 app.listen(PORT, function () {
-  console.log("Port running on ", PORT);
+    console.log("Port running on ", PORT);
 });
